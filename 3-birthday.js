a4d0ba36658ec7acb1b07c619e32d9ab40786cb3
Dmitrii Timofeev <28dngn@gmail.ru>
2024-10-31 23:51:53 +0300

    fix

--- 3-birthday.js
+++ 3-birthday.js
@@ -3,9 +3,8 @@
 const isValidAge = (date) => {
     const dateOfBirth = new Date(date);
     const dateAge14 = new Date(dateOfBirth.getFullYear() + 14, dateOfBirth.getMonth(), dateOfBirth.getDate());
-    const now = new Date();
 
-    return now.getTime() > dateAge14.getTime();
+    return Date.now() > dateAge14.getTime();
 }
 
 console.log(isValidAge('2014-02-10')); // false;
