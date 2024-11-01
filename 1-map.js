7e4456f5c280612b80d787d9a56634f61f705c63
Dmitrii Timofeev <28dngn@gmail.ru>
2024-11-01 18:14:36 +0300

    fix

--- 1-map.js
+++ 1-map.js
@@ -1,14 +1,17 @@
 'use srtict';
 
-let arr = [
-    {id: 1, name: 'Вася'},
-    {id: 2, name: 'Петя'},
-    {id: 1, name: 'Вася'},
-    {id: 2, name: 'Петя'},
+const array = [
     {id: 5, name: 'Софа'},
+    {id: 1, name: 'Вася'},
+    {id: 2, name: 'Петя'},
+    {id: 1, name: 'Вася'},
+    {id: 2, name: 'Петя'},
 ];
 
-const arrId = arr.map(el => el.id);
-arr = [...new Set(arrId)].map(el => arr.find(obj => obj.id === el));
+const getFilteredArr = (array) => {
+    return [...new Set(array.map(item => item.id))]
+                .map(el => array.find(item => item.id === el));
+}
 
-console.log(arr);
+console.log(array);
+console.log(getFilteredArr(array));
